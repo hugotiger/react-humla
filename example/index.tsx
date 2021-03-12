0bba8e6fddc6bcc1269f3ae19638593387e9e414
Hugo Holm <hugo.tiger.holm@gmail.com>
2021-03-12 18:40:23 +0100

    Double quote to fix Linting issue

--- example/index.tsx
+++ example/index.tsx
@@ -1,24 +1,14 @@
-import * as React from 'react';
-import 'react-app-polyfill/ie11';
-import * as ReactDOM from 'react-dom';
-import { useSmartLoading } from '../.';
+import * as React from "react";
+import "react-app-polyfill/ie11";
+import * as ReactDOM from "react-dom";
+import { Button } from "../.";
 
 const App = () => {
-  const [isLoading, setIsLoading] = React.useState(false);
-  const res = useSmartLoading(isLoading);
-
-  React.useEffect(() => {
-    console.log(res);
-  }, res);
-
   return (
     <div>
-      Hej!
-      <button onClick={() => setIsLoading(old => !old)}>Toggle loading</button>
-      <pre>{JSON.stringify(res, null, 2)}</pre>
-      <pre>{JSON.stringify(isLoading, null, 2)}</pre>
+      <Button>Skapa konto</Button>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
