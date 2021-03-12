dfadc49d8c7a2ba09b84c22e645f5f629741ab60
Hugo Holm <hugo.tiger.holm@gmail.com>
2021-03-12 18:32:55 +0100

    Refactoring into separate files

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,7 @@
-import * as React from 'react';
-// https://www.nngroup.com/articles/powers-of-10-time-scales-in-ux/
-// https://www.nngroup.com/articles/response-times-3-important-limits/
-
-interface LoadingOptions {
-  duration: number;
-}
-
-// eslint-disable-next-line prettier/prettier
-// prettier-ignore
-type isSmartLoadingType = boolean;
-type isDelayedType = boolean;
-type SmartLoadingResult = [isSmartLoadingType, isDelayedType];
-
-const DEFAULT_OPTIONS = {
-  duration: 1000,
-};
-
-export function useSmartLoading(
-  isLoading: boolean,
-  options: LoadingOptions = DEFAULT_OPTIONS
-): SmartLoadingResult {
-  const { duration = 1000 } = options;
-
-  const [isSmartLoading, setIsSmartLoading] = React.useState(false);
-  const [isDelayed, setIsDelayed] = React.useState(false);
-
-  React.useEffect(() => {
-    if (!isLoading && isSmartLoading) {
-      setIsSmartLoading(false);
-      return;
-    }
-
-    // Only runs if isLoading is true
-    const timeout = setTimeout(() => setIsSmartLoading(true), duration);
-
-    return () => clearTimeout(timeout);
-  }, [isLoading]);
-
-  React.useEffect(() => {
-    if (!isLoading && isDelayed) {
-      setIsDelayed(false);
-      return;
-    }
-
-    // Only runs if isLoading is true
-    const timeout = setTimeout(() => setIsDelayed(true), 5000);
-
-    return () => clearTimeout(timeout);
-  }, [isLoading]);
-
-  return [isSmartLoading, isDelayed];
-}
+export { Button } from "./components/Button";
+export { default as useArray } from "./hooks/useArray";
+export { default as useHasMounted } from "./hooks/useHasMounted";
+export { default as useLocalStorage } from "./hooks/useLocalStorage";
+export { default as usePrevious } from "./hooks/usePrevious";
+export { default as useSmartLoading } from "./hooks/useSmartLoading";
+export { default as useToggle } from "./hooks/useToggle";
